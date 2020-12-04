3b5ebd4039e315180e5c8859f582e67fa3aa0e8c
bernix-coder <owusubernice.adjoa@gmail.com>
2020-12-04 14:15:41 -0800

    message

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// import FloatingBut from './components/FloatingBut'
+//import FloatingBut from './components/FloatingBut'
 ReactDOM.render(
   <React.StrictMode>
     <App />
